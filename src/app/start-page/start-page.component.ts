bfb66688826633199f172fd4d33763111a8d2d5c
Coo1PeR <coo2per@gmail.com>
2024-05-17 16:54:36 +0300

    feat: add button for additing new user

--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -1,16 +1,25 @@
-import {Component} from '@angular/core';
-import {MatTabsModule} from '@angular/material/tabs';
-import {UsersComponent} from "../users-table/users-table.component";
-import { RouterModule } from "@angular/router";
-
+import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { UsersComponent } from '../users-table/users-table.component';
+import { Router, RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { UserFull } from '../interfaces/userFull';
+import { UserCartComponent } from '../user-cart/user-cart.component';
+import { DashboardService } from '../dashboard.service';
+import { MatDialog } from '@angular/material/dialog';
+import { AddUserComponent } from '../add-user/add-user.component';
 
 @Component({
   selector: 'app-start-page',
   standalone: true,
-  imports: [MatTabsModule, UsersComponent, RouterModule],
+  imports: [MatTabsModule, UsersComponent, RouterModule, MatButtonModule],
   templateUrl: './start-page.component.html',
-  styleUrl: './start-page.component.scss'
+  styleUrl: './start-page.component.scss',
 })
 export class StartPageComponent {
+  constructor(private dialog: MatDialog) {}
 
+  openAddUser() {
+    this.dialog.open(AddUserComponent, {});
+  }
 }
--- src/app/user-cart/user-cart.component.ts
+++ src/app/user-cart/user-cart.component.ts
@@ -1,30 +1,29 @@
-import {Component, Inject, Input, OnInit} from '@angular/core';
-import {ShoppingListComponent} from "../shopping-list/shopping-list.component";
-import {DashboardService} from "../dashboard.service";
-import {Router, RouterModule} from "@angular/router";
-import {MatTableModule} from "@angular/material/table";
-import {MatSort, MatSortModule} from "@angular/material/sort";
-import {MAT_DIALOG_DATA} from "@angular/material/dialog";
-import {UserFull} from "../interfaces/userFull";
-import {MatCardModule} from '@angular/material/card';
-
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { ShoppingListComponent } from '../shopping-list/shopping-list.component';
+import { DashboardService } from '../dashboard.service';
+import { Router, RouterModule } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UserFull } from '../interfaces/userFull';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user-cart',
   standalone: true,
-  imports: [ShoppingListComponent, RouterModule,MatTableModule, MatSortModule, MatCardModule],
+  imports: [
+    ShoppingListComponent,
+    RouterModule,
+    MatTableModule,
+    MatSortModule,
+    MatCardModule,
+  ],
   templateUrl: './user-cart.component.html',
-  styleUrl: './user-cart.component.scss'
+  styleUrl: './user-cart.component.scss',
 })
-
 export class UserCartComponent {
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: UserFull,
-    private dashboardService: DashboardService
-  ) {
-    console.log(data);
-  }
+    private dashboardService: DashboardService,
+  ) {}
 }
-
-
-
